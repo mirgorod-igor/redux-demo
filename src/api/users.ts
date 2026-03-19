4ec0d1706a65dddf6c96a270744f8fad78077644
Игорь Миргород <mirgorod.igor@mail.ru>
2026-03-19 22:07:11 +0600

    fix

--- src/api/users.ts
+++ src/api/users.ts
@@ -3,7 +3,7 @@ import { createAsyncThunk } from '@reduxjs/toolkit'
 export const fetchItemById = createAsyncThunk(
   'items/fetchItemById',
   async (itemId: string) => {
-    const item = await someHttpRequest(itemId)
+    const item = await fetch(itemId)
     return item
   }
 )
